baa32c2abab28c872545493899f35840387efff5
Clay Battle <cbattle1@binghamton.edu>
2023-09-14 13:00:59 -0400

    Working shutdown

--- base/shutdown.c
+++ base/shutdown.c
@@ -6,10 +6,9 @@
    stub functions for the system call user space wrapper functions are provided. 
    REMEMBER to disable the stub functions (by commenting the following macro) to 
    allow your implementation to work properly. */
-#define STUB_FUNCS
-#ifdef STUB_FUNCS
-void shutdown(void) {}
-#endif
+/*void shutdown(void) {
+    sys_shutdown();
+}*/
 
 
 /* IMPORTANT INSTRUCTION: the code below should not be changed. 
